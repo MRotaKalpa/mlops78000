283daaa06b887db61969e5b960cc1dccce67ff2b
Manuel Rota <manuel.rota@kalpa.it>
2023-05-10 10:14:56 +0200

    feat: add training scripts and files

---
+++ datasets/kalpa_topviewedpeople.py
@@ -0,0 +1,145 @@
+import os
+import pandas as pd
+import torch
+from torchvision import transforms
+from sklearn.preprocessing import LabelEncoder
+import ai8x
+import re
+
+import torchvision
+from PIL import Image
+from pathlib import Path
+from torch.utils.data import Dataset
+
+enabled_classes = ('negative', 'person',)
+
+class TopViewedPeople(Dataset):
+
+    def __init__(self, data_dir, train=True, transform=None):
+
+        self.transform = transform
+        self.train = train
+        
+        download = not os.path.exists(os.path.join(data_dir, 'CIFAR10'))
+        
+        if self.train:
+            cifar_transform = transform['cifar10_train']
+        else:
+            cifar_transform = transform['cifar10_test']
+        self.cifar_dataset = torchvision.datasets.CIFAR10(
+            root=os.path.join(data_dir, 'CIFAR10'),
+            train=self.train, 
+            download=download, 
+            transform=cifar_transform)
+
+        images = []
+        labels = []
+
+        img_folder = 'train_subset_canteen' if self.train else 'test_canteen'
+        for img_path in (Path(data_dir) / img_folder).glob('*.png'):
+            lbl = "person"
+            if lbl in enabled_classes:
+                images.append(img_path)
+                labels.append(1)
+
+        img_folder = 'train' if self.train else 'test'
+        for img_path in (Path(data_dir) / img_folder).glob('*.jpg'):
+            # lbl = img_path.parent.name
+            lbl = "person"
+            if lbl in enabled_classes:
+                    images.append(img_path)
+                    labels.append(1)
+
+        img_folder = 'train_subset_negatives' if self.train else 'test_negatives'
+        for img_path in (Path(data_dir) / img_folder).glob('*.png'):
+            lbl = "negative"
+            if lbl in enabled_classes:
+                    images.append(img_path)
+                    labels.append(0)
+
+        data = {'images': images, 'labels': labels}
+
+        self.img_data = pd.DataFrame(data)
+
+    def __len__(self):
+        return len(self.cifar_dataset) + len(self.img_data)
+
+    def __getitem__(self, index):
+        if index < 0:
+            index += len(self)
+
+        if 0 <= index < len(self.cifar_dataset):
+            return self.cifar_dataset[index]
+
+        # shift due to the concatenation with cifar10
+        index = index - len(self.cifar_dataset)
+        img_name = os.path.join(self.img_data.loc[index, 'images'])
+
+        image = Image.open(img_name)
+        image = image.resize((40, 40))
+        label = self.img_data.loc[index, 'labels']
+        if self.transform is not None:
+            if self.train:
+                image = self.transform['train'](image)
+            else:
+                image = self.transform['test'](image)
+        return image, label + 10  # + 10 is due to the concatenation with cifar10
+
+
+def top_viewed_people_get_datasets(data, load_train=True, load_test=True):
+    
+    (data_dir, args) = data
+    print(data_dir)
+    
+    train_dataset = None
+    test_dataset = None
+   
+    data_transforms = {
+        'cifar10_train': transforms.Compose([
+             transforms.RandomCrop(32, padding=4),
+             transforms.RandomHorizontalFlip(),
+             transforms.ToTensor(),
+             ai8x.normalize(args=args)
+        ]),
+        'cifar10_test': transforms.Compose([
+             transforms.ToTensor(),
+             ai8x.normalize(args=args)
+        ]),
+        'train': transforms.Compose([
+             transforms.RandomCrop(32),
+             transforms.RandomAffine(degrees=10, translate=None, scale=(0.8, 1.2)),
+             transforms.ColorJitter(brightness=0.3, contrast=0.2, saturation=0.1, hue=0.03),
+             transforms.GaussianBlur(kernel_size=(3,3), sigma=(0.1, 5)),
+             transforms.RandomHorizontalFlip(),
+             transforms.RandomVerticalFlip(),
+             transforms.ToTensor(),
+             ai8x.normalize(args=args)
+        ]),
+        'test': transforms.Compose([
+             transforms.CenterCrop((32, 32)),
+             transforms.ToTensor(),
+             ai8x.normalize(args=args)
+        ])
+    }
+    
+    if load_train:
+        train_dataset = TopViewedPeople(data_dir, train=True, transform=data_transforms)
+    
+    if load_test:
+        test_dataset = TopViewedPeople(data_dir, train=False, transform=data_transforms)
+        
+        if args.truncate_testset:
+            test_dataset.data = test_dataset.data[:1]
+    
+    return train_dataset, test_dataset
+
+
+datasets = [
+    {
+        'name': 'top_viewed_people',
+        'input': (3, 32, 32),
+        'output': ('plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse',
+                   'ship', 'truck') + enabled_classes,
+        'loader': top_viewed_people_get_datasets,
+    }
+]
---
+++ train.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+SCRIPT_PATH=$(dirname "$(realpath "$0")")
+KALPA_PATH=$SCRIPT_PATH
+TRAINING_PATH=$SCRIPT_PATH/ai8x-training
+DATASET_NAME=top_viewed_people
+
+# Sync user data and datasets
+# TODO esiste un modo migliore senza fare la copia?
+rsync -avx $KALPA_PATH/datasets/ $TRAINING_PATH/datasets/
+rsync -avx $KALPA_PATH/models/ $TRAINING_PATH/models/
+
+# Start training
+cd $TRAINING_PATH
+. bin/activate
+python3 train.py --epochs 100 --data $KALPA_PATH/data --dataset $DATASET_NAME --model ai85ressimplenet --compress $KALPA_PATH/schedule-kalpa.yaml --optimizer Adam --lr 0.001 --deterministic --confusion --param-hist --embedding --device MAX78000 --cpu --enable-tensorboard --out-dir $KALPA_PATH/net --qat-policy $KALPA_PATH/qat_kalpa_policy.yaml
+deactivate
